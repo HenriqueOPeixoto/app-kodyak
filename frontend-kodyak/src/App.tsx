9c4600bf77cf7a57dacc577dcc555f166439d59b
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2024-05-05 19:53:22 +0000

    Criar componentes para cadastro e dashboard

--- frontend-kodyak/src/App.tsx
+++ frontend-kodyak/src/App.tsx
@@ -1,7 +1,8 @@
 import './App.css'
 import './components/Sidebar/Sidebar.css'
 import Sidebar from './components/Sidebar/Sidebar'
-
+import Cadastro from './components/Content/Cadastro';
+import Dashboard from './components/Content/Dashboard';
 
 import '@fontsource/roboto/400.css';
 
@@ -15,6 +16,7 @@ function App() {
         <div className='Sidebar'><Sidebar /></div>
         <div className='Main'>
           {/* O conteúdo da página atual será carregado aqui */}
+          <Dashboard />
         </div>
       </div>
     </>
