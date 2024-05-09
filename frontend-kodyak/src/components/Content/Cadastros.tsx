1c83e104c5de6ebb07b9bdff75a935ca671f68d0
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2024-05-09 01:05:58 +0000

    Criar botões da tela de cadastro

--- frontend-kodyak/src/components/Content/Cadastros.tsx
+++ frontend-kodyak/src/components/Content/Cadastros.tsx
@@ -1,6 +1,26 @@
+import { Button } from '@mui/material'
+import PeopleIcon from '@mui/icons-material/People';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+
+import './Cadastros.css'
+
 function Cadastros() {
   return (
-    <div>Cadastros</div>
+    <>
+      <div className="Title">
+        Cadastros
+        <hr />
+      </div>
+      <div className='BotoesCadastro'>
+        <Button startIcon={<AccountBalanceIcon />}>Bancos</Button>
+        <Button startIcon={<PeopleIcon />}>Clientes</Button>
+        <Button startIcon={<ShoppingBagIcon />}>Família de Produtos</Button>
+      </div>
+      <div className='ListaCadastros'>
+        
+      </div>
+    </>
   )
 }
 
