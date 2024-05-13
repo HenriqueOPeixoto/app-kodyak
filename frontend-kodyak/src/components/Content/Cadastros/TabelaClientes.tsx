d0413f19c7f285ebb7a91907023f6b1855618d88
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2024-05-13 15:18:01 +0000

    Inserir botão Incluir e posicionar na direita

--- frontend-kodyak/src/components/Content/Cadastros/TabelaClientes.tsx
+++ frontend-kodyak/src/components/Content/Cadastros/TabelaClientes.tsx
@@ -1,10 +1,13 @@
-import { Paper, Table, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
+import { Button, Paper, Table, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
 
 import '../Cadastros.css'
 
 function TabelaClientes() {
   return (
     <div className="TabelaClientes">
+      <div className="ContainerBtnIncluir">
+        <Button className="BtnIncluirCliente" variant="contained" color="success">Incluir</Button>
+      </div>
       <TableContainer component={Paper}>
         <TableHead>
           <TableRow>
@@ -15,7 +18,7 @@ function TabelaClientes() {
           <TableRow>
 
             {/* TODO: Fazer scrollbar horizontal parar de aparecer. O conteúdo deve caber na página */}
-            <TableCell>Teste da Silvaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</TableCell>
+            <TableCell>Teste da Silva</TableCell>
             <TableCell>123.456.789-00</TableCell>
             <TableCell>Rua dos Testes, 123W</TableCell>
           </TableRow>
