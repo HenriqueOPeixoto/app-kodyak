cde019aaf5d7ab109404c7129d3a09a0b61dcac4
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2024-05-13 15:25:33 +0000

    Nova rota de cadastro cliente

--- frontend-kodyak/src/App.tsx
+++ frontend-kodyak/src/App.tsx
@@ -3,6 +3,8 @@ import './components/Sidebar/Sidebar.css'
 import Sidebar from './components/Sidebar/Sidebar'
 import Cadastro from './components/Content/Cadastros';
 import Dashboard from './components/Content/Dashboard';
+import Clientes from './components/Content/Cadastros/Clientes';
+
 
 import '@fontsource/roboto/400.css';
 import { Route, Routes } from 'react-router-dom'
@@ -19,6 +21,7 @@ function App() {
           <Routes>
             <Route path='/cadastros' Component={Cadastro}></Route>
             <Route path='/dashboard' Component={Dashboard}></Route>
+            <Route path='/cadastros/novo_cliente' Component={Clientes}></Route>
           </Routes>
         </div>
       </div>
