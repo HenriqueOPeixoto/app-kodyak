581a7455d99a6040ef087c3fc496303189015767
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2024-05-26 19:26:46 +0000

    Preencher cadastro de motoristas com cards

--- frontend-kodyak/src/components/Cadastros/TabelaMotoristas.tsx
+++ frontend-kodyak/src/components/Cadastros/TabelaMotoristas.tsx
@@ -1,9 +1,50 @@
-import { Button, Paper, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
+import { Button, Card, CardActionArea, CardContent, Grid, Typography } from "@mui/material"
 
 import './styles/Cadastros.css'
 import { Link } from "react-router-dom"
+import React, { useEffect, useState } from "react"
+import axios from "axios"
 
-function TabelaMotoristas() {
+interface Motorista {
+  id: number
+  nome:string
+  telefone: string
+  placa: string
+  vinculo: string
+  tp_caminhao: number
+}
+
+const CardMotorista: React.FC<{ motorista:Motorista }> = ({ motorista }) => {
+  return (
+    <Card>
+      <CardActionArea>
+        <CardContent>
+          <Typography>#{motorista.id}</Typography>
+          <Typography variant="h5">{motorista.nome}</Typography>
+          <Typography>{motorista.placa}</Typography>
+          <Typography>{motorista.telefone}</Typography>
+          <Typography>{motorista.vinculo}</Typography>
+          <Typography>{motorista.tp_caminhao}</Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  )
+}
+
+const TabelaMotoristas: React.FC = () => {
+
+  const [motorista, setMotoristas] = useState<Motorista[]>([])
+
+  useEffect(() => {
+    axios.get<Motorista[]>('http://localhost:5174/api/motoristas')
+      .then(response => {
+        setMotoristas(response.data);
+      })
+      .catch(error => {
+        console.error("There was an error fetching the drivers!", error);
+      });
+  }, []);
+
   return (
     <div className="TabelaMotoristas">
       <div className="ContainerBtnIncluir">
@@ -11,24 +52,13 @@ function TabelaMotoristas() {
           <Button className="BtnIncluir" variant="contained" color="success">Incluir</Button>
         </Link>
       </div>
-      <TableContainer component={Paper}>
-        <TableHead>
-          <TableRow>
-            <TableCell>Nome</TableCell>
-            <TableCell>Placa</TableCell>
-            <TableCell>Telefone</TableCell>
-            <TableCell>Vínculo - Caminhão</TableCell>
-          </TableRow>
-          <TableRow>
-
-            {/* TODO: Fazer scrollbar horizontal parar de aparecer. O conteúdo deve caber na página */}
-            <TableCell>Teste da Silva</TableCell>
-            <TableCell>AAA1A11</TableCell>
-            <TableCell>(65) 91234-5678</TableCell>
-            <TableCell>?????</TableCell>
-          </TableRow>
-        </TableHead>
-      </TableContainer>
+      <Grid container spacing={2} style={{ overflowY: 'auto', height: '80vh' }}>
+        {motorista.map((motorista) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={motorista.id}>
+            <CardMotorista motorista={motorista} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   )
 }
