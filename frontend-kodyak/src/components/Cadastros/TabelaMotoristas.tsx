775f51f618eb81f13bc6e3570378843a47a71966
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2024-07-20 18:46:01 +0000

    Adicionar filtros de pesquisa de motorista

--- frontend-kodyak/src/components/Cadastros/TabelaMotoristas.tsx
+++ frontend-kodyak/src/components/Cadastros/TabelaMotoristas.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, CardActionArea, CardContent, Grid, Typography } from "@mui/material"
+import { Button, Card, CardActionArea, CardContent, FormControl, FormControlLabel, FormLabel, Grid, Radio, RadioGroup, TextField, Typography } from "@mui/material"
 
 import './styles/Cadastros.css'
 import { Link } from "react-router-dom"
@@ -7,27 +7,27 @@ import axios from "axios"
 
 interface Motorista {
   id: number
-  nome:string
+  nome: string
   telefone: string
   placa: string
   vinculo: string
   tp_caminhao: number
 }
 
-const CardMotorista: React.FC<{ motorista:Motorista }> = ({ motorista }) => {
+const CardMotorista: React.FC<{ motorista: Motorista }> = ({ motorista }) => {
   return (
     <Card variant="outlined">
-      <Link to={`/cadastros/editar_motorista/${motorista.id}`} style={{ textDecoration: 'none', color: 'inherit'}}>
-      <CardActionArea>
-        <CardContent>
-          <Typography variant="subtitle2">#{motorista.id}</Typography>
-          <Typography variant="h6">{motorista.nome}</Typography>
-          <Typography>Placa: {motorista.placa}</Typography>
-          <Typography>Telefone: {motorista.telefone}</Typography>
-          <Typography>Vinculo: {motorista.vinculo}</Typography>
-          <Typography>Caminhão: {motorista.tp_caminhao}</Typography>
-        </CardContent>
-      </CardActionArea>
+      <Link to={`/cadastros/editar_motorista/${motorista.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+        <CardActionArea>
+          <CardContent>
+            <Typography variant="subtitle2">#{motorista.id}</Typography>
+            <Typography variant="h6">{motorista.nome}</Typography>
+            <Typography>Placa: {motorista.placa}</Typography>
+            <Typography>Telefone: {motorista.telefone}</Typography>
+            <Typography>Vinculo: {motorista.vinculo}</Typography>
+            <Typography>Caminhão: {motorista.tp_caminhao}</Typography>
+          </CardContent>
+        </CardActionArea>
       </Link>
     </Card>
   )
@@ -43,13 +43,26 @@ const TabelaMotoristas: React.FC = () => {
         setMotoristas(response.data);
       })
       .catch(error => {
-        console.error("There was an error fetching the drivers!", error);
+        console.error("Erro ao listar os motoristas: ", error);
       });
   }, []);
 
   return (
     <div className="TabelaMotoristas">
       <div className="ContainerBtnIncluir">
+        <TextField className="TxtPesquisarMotorista" id="pesquisar-motorista" label="Pesquisar" variant="standard" />
+        <FormControl>
+          <FormLabel id="ativo-radio-button">Filtros</FormLabel>
+          <RadioGroup
+            row
+            aria-labelledby="ativo-radio-buttons-group-label"
+            defaultValue="ativo"
+            name="radio-buttons-group"
+          >
+            <FormControlLabel value="ativo" control={<Radio />} label="Ativo" />
+            <FormControlLabel value="inativo" control={<Radio />} label="Inativo" />
+          </RadioGroup>
+        </FormControl>
         <Link to='/cadastros/novo_motorista'>
           <Button className="BtnIncluir" variant="contained" color="success">Incluir</Button>
         </Link>
