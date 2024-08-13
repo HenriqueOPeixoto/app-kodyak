db50aea4317de8d8d088e39e1c7122ef1e50d721
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2024-08-13 18:25:30 +0000

    Importar rota de criação de produto

--- backend-kodyak/index.js
+++ backend-kodyak/index.js
@@ -7,6 +7,7 @@ const motoristaRoutes = require('./routes/MotoristaRoutes');
 const usuarioRoutes = require('./routes/UsuarioRoutes');
 const nivelAcessoRoutes = require('./routes/NivelAcessoRoutes')
 const familiaProdutosRoutes = require('./routes/FamiliaProdutosRoutes')
+const produtoRoutes = require('./routes/ProdutoRoutes')
 
 const app = express();
 
@@ -19,6 +20,7 @@ const port = process.env.PORT || 5174;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use('/api/produtos', produtoRoutes)
 app.use('/api/familia_produtos', familiaProdutosRoutes)
 app.use('/api/nivel_acesso', nivelAcessoRoutes)
 app.use('/api/motoristas', motoristaRoutes);
