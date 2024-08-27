c125c4370671646a4d668c702b80fd8d4139f514
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2024-08-27 19:44:02 +0000

    Adicionar rota de clientes

--- backend-kodyak/index.js
+++ backend-kodyak/index.js
@@ -8,6 +8,7 @@ const usuarioRoutes = require('./routes/UsuarioRoutes');
 const nivelAcessoRoutes = require('./routes/NivelAcessoRoutes')
 const familiaProdutosRoutes = require('./routes/FamiliaProdutosRoutes')
 const produtoRoutes = require('./routes/ProdutoRoutes')
+const clienteRoutes = require('./routes/ClienteRoutes')
 
 const app = express();
 
@@ -20,6 +21,7 @@ const port = process.env.PORT || 5174;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use('/api/clientes', clienteRoutes)
 app.use('/api/produtos', produtoRoutes)
 app.use('/api/familia_produtos', familiaProdutosRoutes)
 app.use('/api/nivel_acesso', nivelAcessoRoutes)
