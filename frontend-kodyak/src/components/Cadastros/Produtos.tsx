c25973c568c3943dc30ae95479907baaa026ecb6
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2024-08-27 17:50:48 +0000

    remover importação não utilizada

--- frontend-kodyak/src/components/Cadastros/Produtos.tsx
+++ frontend-kodyak/src/components/Cadastros/Produtos.tsx
@@ -1,7 +1,7 @@
 // TODO: Corrigir família de produtos ao carregar a página
 // TODO: Corrigir Sidebar
 
-import { Box, Button, FormControl, InputAdornment, InputLabel, MenuItem, Select, SelectChangeEvent, Snackbar, TextField } from "@mui/material"
+import { Box, Button, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, Snackbar, TextField } from "@mui/material"
 import { Link, useParams } from "react-router-dom"
 import { useEffect, useState } from "react";
 import { NumericFormat } from "react-number-format"
