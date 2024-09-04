1edfdb9c93c9fbfc01743e18cb8e61323dfc7860
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2024-09-04 19:00:01 +0000

    Busca os dados de cliente no backend

--- frontend-kodyak/src/components/Cadastros/Clientes.tsx
+++ frontend-kodyak/src/components/Cadastros/Clientes.tsx
@@ -1,167 +1,226 @@
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import { Box, Button, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, Snackbar, TextField } from "@mui/material"
+import { Link, useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { NumericFormat } from "react-number-format"
+
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import SaveIcon from '@mui/icons-material/Save';
+import { DoNotDisturb } from '@mui/icons-material';
+import axios from "axios";
+
+import DialogInativar from "./Dialogs/DialogInativar";
+
+import './styles/Produtos.css'
+
+const backendBaseURL = import.meta.env.VITE_BACKEND_BASE_URL
+
 
 export default function Clientes() {
+  const { id } = useParams()
+  const [razaoSocial, setRazaoSocial] = useState('')
+  const [nome, setNome] = useState('')
+  const [cnpj, setCnpj] = useState('')
+  const [cpf, setCpf] = useState('')
+  const [inscricaoEstadual, setInscricaoEstadual] = useState('')
+  const [telefoneFixo, setTelefoneFixo] = useState('')
+  const [telefoneCelular, setTelefoneCelular] = useState('')
+  const [email, setEmail] = useState('')
+  const [cep, setCep] = useState('')
+  const [logradouro, setLogradouro] = useState('')
+  const [numero, setNumero] = useState('')
+  const [bairro, setBairro] = useState('')
+  const [cidade, setCidade] = useState('')
+  const [estado, setEstado] = useState('')
+  const [inativo, setInativo] = useState(false)
+
+  const [dialogOpen, setDialogOpen] = useState(false)
+  const [snackOpen, setSnackOpen] = useState(false)
+  const [snackMessage, setSnackMessage] = useState('')
+  const [btnInativarText, setBtnInativarText] = useState('Inativar')
+
+  useEffect(() => {
+    if (id) {
+      axios.get(`${backendBaseURL}/api/clientes/${id}`)
+        .then(response => {
+          if (response.data.length > 0) {
+            const {
+              razao_social,
+              nome,
+              cnpj,
+              cpf,
+              inscricao_estadual,
+              telefone_fixo,
+              telefone_celular,
+              email,
+              cep,
+              logradouro,
+              numero,
+              bairro,
+              cidade,
+              estado,
+              inativo
+            } = response.data[0]
+
+            setRazaoSocial(razao_social)
+            setNome(nome)
+            setCnpj(cnpj)
+            setInscricaoEstadual(inscricao_estadual)
+            setTelefoneFixo(telefone_fixo)
+            setTelefoneCelular(telefone_celular)
+            setEmail(email)
+            setCep(cep)
+            setLogradouro(logradouro)
+            setNumero(numero)
+            setBairro(bairro)
+            setCidade(cidade)
+            setEstado(estado)
+            setInativo(inativo)
+          }
+        })
+        .catch(error => {
+          console.log('Não foi possível carregar dados do produto: ', error)
+        })
+    }
+  }, [id])
+
+  const handleSubmit = () => {
+    const formData = {
+      razao_social: razaoSocial,
+      nome,
+      cnpj,
+      cpf,
+      inscricao_estadual: inscricaoEstadual,
+      telefone_fixo: telefoneFixo,
+      telefone_celular: telefoneCelular,
+      email,
+      cep,
+      logradouro,
+      numero,
+      bairro,
+      cidade,
+      estado,
+      inativo
+    }
+
+    if (id) {
+      axios.put(`${backendBaseURL}/api/clientes/${id}`, formData)
+        .then(() => {
+          handleAbrirSnack('Cliente atualizado com sucesso!')
+        })
+        .catch(() => {
+          handleAbrirSnack('Ocorreu um erro ao atualizar o cliente.')
+        })
+    } else {
+      axios.post(`${backendBaseURL}/api/clientes/`, formData)
+        .then(() => {
+          handleAbrirSnack('Cliente cadastrado com sucesso.')
+
+        })
+        .catch(() => {
+          handleAbrirSnack('Não foi possível cadastrar o cliente.')
+        })
+
+        setRazaoSocial('')
+        setNome('')
+        setCnpj('')
+        setInscricaoEstadual('')
+        setTelefoneFixo('')
+        setTelefoneCelular('')
+        setEmail('')
+        setCep('')
+        setLogradouro('')
+        setNumero('')
+        setBairro('')
+        setCidade('')
+        setEstado('')
+        setInativo(false)
+    }
+
+  }
+
+  const handleAbrirDialogInativar = () => {
+    setDialogOpen(true)
+  }
+
+  const handleFecharDialogInativar = () => {
+    setDialogOpen(false)
+  }
+
+  const handleAbrirSnack = (message: string) => {
+    setSnackMessage(message)
+    setSnackOpen(true)
+  }
+
+  const handleFecharSnack = () => {
+    setSnackOpen(false)
+  }
+
+  const handleConfirmarDialogInativar = () => {
+    const newInativo = !inativo; // Toggle inativo status
+    axios.patch(`${backendBaseURL}/api/produtos/${id}/alterarStatus`, { inativo: newInativo })
+      .then(response => {
+        handleAbrirSnack(response.data);
+        setInativo(newInativo);
+        setDialogOpen(false);
+      })
+      .catch(error => {
+        console.error('Ocorreu um erro ao inativar o produto: ', error);
+        handleAbrirSnack('Ocorreu um erro ao inativar o produto.');
+      });
+  };
+
+  useEffect(() => {
+    if (inativo) {
+      setBtnInativarText('Ativar')
+    } else {
+      setBtnInativarText('Inativar')
+    }
+  }, [inativo])
+
+
   return (
     <Box
       component="form"
       sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
+        '& .MuiTextField-root': { m: 1, width: '50%' },
       }}
       noValidate
       autoComplete="off"
     >
+      <div className="Title">
+        Cadastro de Clientes
+        <hr />
+      </div>
+      <Link to='/cadastros'>
+        <Button startIcon={<ArrowBackIcon />} color='error'>Voltar</Button>
+      </Link>
       <div>
         <TextField
-          required
-          id="outlined-required"
-          label="Required"
-          defaultValue="Hello World"
-        />
-        <TextField
-          disabled
-          id="outlined-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-        />
-        <TextField
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-        />
-        <TextField
-          id="outlined-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
-        />
-        <TextField
-          id="outlined-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <TextField id="outlined-search" label="Search field" type="search" />
-        <TextField
-          id="outlined-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
+          label="Nome"
+          value={nome}
         />
       </div>
-      <div>
-        <TextField
-          required
-          id="filled-required"
-          label="Required"
-          defaultValue="Hello World"
-          variant="filled"
-        />
-        <TextField
-          disabled
-          id="filled-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-          variant="filled"
-        />
-        <TextField
-          id="filled-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="filled"
-        />
-        <TextField
-          id="filled-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
-          variant="filled"
-        />
-        <TextField
-          id="filled-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="filled"
-        />
-        <TextField
-          id="filled-search"
-          label="Search field"
-          type="search"
-          variant="filled"
-        />
-        <TextField
-          id="filled-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          variant="filled"
-        />
+      <div className='FormButtons'>
+        <Button startIcon={<SaveIcon />}
+          variant='contained'
+          color='success'
+          onClick={() => { handleSubmit() }}
+        >{id ? 'Atualizar' : 'Gravar'}</Button>
+        <Button startIcon={<DoNotDisturb />}
+          variant='contained'
+          color='error'
+          onClick={handleAbrirDialogInativar}>{btnInativarText}</Button>
       </div>
-      <div>
-        <TextField
-          required
-          id="standard-required"
-          label="Required"
-          defaultValue="Hello World"
-          variant="standard"
-        />
-        <TextField
-          disabled
-          id="standard-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-          variant="standard"
-        />
-        <TextField
-          id="standard-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="standard"
-        />
-        <TextField
-          id="standard-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
-          variant="standard"
-        />
-        <TextField
-          id="standard-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="standard"
-        />
-        <TextField
-          id="standard-search"
-          label="Search field"
-          type="search"
-          variant="standard"
-        />
-        <TextField
-          id="standard-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          variant="standard"
-        />
-      </div>
+      <DialogInativar
+        open={dialogOpen}
+        handleClose={handleFecharDialogInativar}
+        handleConfirm={handleConfirmarDialogInativar} />
+      <Snackbar
+        open={snackOpen}
+        autoHideDuration={6000}
+        onClose={handleFecharSnack}
+        message={snackMessage}
+      />
     </Box>
-  );
+  )
 }
