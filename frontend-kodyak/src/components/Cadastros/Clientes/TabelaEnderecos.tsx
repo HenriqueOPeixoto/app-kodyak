b6220db6b5a4262d7f950984493012669037e16d
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2024-11-17 15:14:14 +0000

    Criar componente de listagem de enderecos

---
+++ frontend-kodyak/src/components/Cadastros/Clientes/TabelaEnderecos.tsx
@@ -0,0 +1,105 @@
+import { Box, Button, Card, CardActionArea, CardContent, FormControl, FormControlLabel, FormLabel, Grid, Radio, RadioGroup, Typography } from "@mui/material"
+
+import '../styles/Cadastros.css'
+import { Link } from "react-router-dom"
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+
+interface TabelaEnderecosProps {
+    clienteId: string;
+  }
+
+interface Enderecos {
+    id: number
+    inscricao_estadual: string
+    telefone_fixo: string
+    telefone_celular: string
+    email: string
+    cep: string
+    logradouro: string
+    numero: string
+    bairro: string
+    cidade: string
+    estado: string
+    cliente: string
+}
+
+const CardEnderecos: React.FC<{ enderecos: Enderecos }> = ({ enderecos }) => {
+    return (
+        <Card className="CardEndereco" variant="outlined">
+            <Link to={`/cadastros/editar_endereco/${enderecos.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                <CardActionArea>
+                    <CardContent>
+                        <Typography variant="h6">{enderecos.inscricao_estadual}</Typography>
+                        <Typography variant="h6">{enderecos.logradouro}, {enderecos.numero} - {enderecos.bairro}</Typography>
+                        <Typography variant="h6">{enderecos.cidade}, {enderecos.estado}</Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Link>
+        </Card>
+    )
+}
+
+const backendBaseURL = import.meta.env.VITE_BACKEND_BASE_URL
+
+const TabelaEnderecos: React.FC<TabelaEnderecosProps> = ({ clienteId }) => {
+
+    const [enderecos, setEnderecos] = useState<Enderecos[]>([])
+    const [inscricaoEstadual] = useState<string>('')
+    const [inativo, setInativo] = useState<boolean>(false)
+
+
+    useEffect(() => {
+        axios.get<Enderecos[]>(`${backendBaseURL}/api/clientes_enderecos`, {
+            params: {
+                "cliente": clienteId,
+                "inativo": inativo
+            }
+        }
+        )
+            .then(response => {
+                console.log('end: ' + response.data)
+                console.log(clienteId)
+                setEnderecos(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, [inscricaoEstadual, inativo]);
+
+
+    const handleInativoRadioButtonChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInativo(event.target.value === 'true')
+    }
+
+    return (
+        <Box>
+            <div className="TabelaEnderecos">
+                <div className="ContainerFiltros">
+                    <FormControl>
+                        <FormLabel id="ativo-radio-button">Filtros</FormLabel>
+                        <RadioGroup defaultValue="ativo" row onChange={handleInativoRadioButtonChange}>
+                            <FormControlLabel value="false" control={<Radio />} label="Ativo" />
+                            <FormControlLabel value="true" control={<Radio />} label="Inativo" />
+                        </RadioGroup>
+                    </FormControl>
+                    <div className="Botoes">
+                        {/* <Button className="BtnPesquisar" variant="contained">Pesquisar</Button> */}
+                        <Link to='/cadastros/novo_endereco'>
+                            <Button className="BtnIncluir" variant="contained" color="success">Incluir</Button>
+                        </Link>
+                    </div>
+                </div>
+                <Grid container spacing={2} style={{ overflowY: 'auto', height: '80vh' }}>
+                    {enderecos.map((enderecos) => (
+                        <Grid item xs={12} key={enderecos.id}>
+                            <CardEnderecos enderecos={enderecos} />
+                        </Grid>
+                    ))}
+                </Grid>
+            </div>
+        </Box>
+    )
+}
+
+export default TabelaEnderecos
