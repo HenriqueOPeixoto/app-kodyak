7481bb84fa0fa1cf888b4a05ed44f2f8fe5e2d51
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2025-02-18 19:25:36 +0000

    Cadastrar toda a hierarquia de localidades do IBGE no banco de dados

--- backend-kodyak/importador_ibge.js
+++ backend-kodyak/importador_ibge.js
@@ -1,9 +1,21 @@
 const pool = require('./postgres').pool
+const regioes = require('./localidades_ibge/regioes.json')
 const estados = require('./localidades_ibge/estados.json')
+const mesorregioes = require('./localidades_ibge/mesorregioes.json')
+const microrregioes = require('./localidades_ibge/microrregioes.json')
 const municipios = require('./localidades_ibge/municipios.json')
 
 function cadastrarLocalidades() {
-    console.log('Aguarde... Cadastrando estados no banco de dados.')
+    console.log('=== Aguarde... Cadastrando regioes no banco de dados. ===')
+    regioes.map((regiao) => {
+        pool.query(
+            'INSERT INTO REGIOES_BRASILEIRAS (id, sigla, nome) VALUES ($1, $2, $3)',
+            [regiao.id, regiao.sigla, regiao.nome]
+        )
+        .then(() => { console.log(regiao.nome + ' cadastrado com sucesso!') })
+        .catch((error) => { console.log('Ocorreu um erro ao cadastrar ' + regiao.nome + ' -> ' + error) })
+    })
+
     estados.map((estado) => {
         pool.query(
             'INSERT INTO UNIDADES_FEDERATIVAS (id, sigla, nome, regiao) VALUES ($1, $2, $3, $4)',
@@ -11,6 +23,39 @@ function cadastrarLocalidades() {
         )
         .then(() => { console.log(estado.nome + ' cadastrado com sucesso!') })
         .catch((error) => { console.log('Ocorreu um erro ao cadastrar ' + estado.nome + ' -> ' + error) })
+    })
+
+    console.log('\n')
+    console.log('=== Cadastrando mesorregiões ===')
+    mesorregioes.map((mesorregiao) => {
+        pool.query(
+            'INSERT INTO MESORREGIOES (id, nome, unidade_federativa) VALUES ($1, $2, $3)',
+            [mesorregiao.id, mesorregiao.nome, mesorregiao.UF.id]
+        )
+        .then(() => { console.log(mesorregiao.nome + ' cadastrado com sucesso!') })
+        .catch((error) => { console.log('Ocorreu um erro ao cadastrar ' + mesorregiao.nome + ' -> ' + error) })
+    })
+
+    console.log('\n')
+    console.log('=== Cadastrando microrregiões ===')
+    microrregioes.map((microrregiao) => {
+        pool.query(
+            'INSERT INTO MICRORREGIOES (id, nome, mesorregiao) VALUES ($1, $2, $3)',
+            [microrregiao.id, microrregiao.nome, microrregiao.mesorregiao.id]
+        )
+        .then(() => { console.log(microrregiao.nome + ' cadastrado com sucesso!') })
+        .catch((error) => { console.log('Ocorreu um erro ao cadastrar ' + microrregiao.nome + ' -> ' + error) })
+    })
+
+    console.log('\n')
+    console.log('=== Cadastrando municípios ===')
+    municipios.map((municipio, index) => {
+        pool.query(
+            'INSERT INTO MUNICIPIOS (id, nome, microrregiao) VALUES ($1, $2, $3)',
+            [municipio.id, municipio.nome, municipio.microrregiao.id]
+        )
+        .then(() => { console.log(index + ': ' + municipio.nome + ' cadastrado com sucesso!') })
+        .catch((error) => { console.log('Ocorreu um erro ao cadastrar ' + municipio.nome + ' -> ' + error) })
     })
 }
 
