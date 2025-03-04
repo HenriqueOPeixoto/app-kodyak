533952aaa75c6a84d0eec756aa2f89c5a3ea9322
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2025-03-04 20:19:21 +0000

    Adicionar função para verificar se um token de acesso é válido

---
+++ backend-kodyak/middleware/Authorization.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+
+function authenticateToken(req, res, next) {
+    // Bearer [TOKEN]
+    const authHeader =  req.headers['authorization']
+
+    const token = authHeader && authHeader.split(' ')[1] // Primeiro verifica se != null e depois do AND separa em ('Bearer', TOKEN)
+
+    if (token == null) {
+        return res.status(401).send('Token nulo')
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, user) => {
+        if (error) { return res.status(403).send(error)}
+        
+        req.user = user
+        next()
+    })
+}
+
+module.exports = authenticateToken
