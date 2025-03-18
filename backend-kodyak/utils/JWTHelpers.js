ac52c4bc01ad7949a6e6312b6e1a85364bc00cea
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2025-03-18 20:05:21 +0000

    Usar apenas o id para gerar os tokens

--- backend-kodyak/utils/JWTHelpers.js
+++ backend-kodyak/utils/JWTHelpers.js
@@ -1,7 +1,10 @@
 const jwt = require('jsonwebtoken')
 
 function jwtTokens({id, nome, email}) {
-    const usuario = { id, nome, email }
+    // Enquanto não for necessário passar mais informações, o token será gerado apenas com
+    // o ID no payload.
+    //const usuario = { id, nome, email }
+    const usuario = { id }
 
     const accessToken = jwt.sign(usuario, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '15m'})
     const refreshToken = jwt.sign(usuario, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '1d'})
