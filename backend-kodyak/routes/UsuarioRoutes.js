8a6648f88e1104028ffc1f80a9d9db07c21c128c
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2025-05-08 18:21:09 +0000

    Corrigir conflito entre rotas

--- backend-kodyak/routes/UsuarioRoutes.js
+++ backend-kodyak/routes/UsuarioRoutes.js
@@ -3,19 +3,21 @@ const router = express.Router();
 const UsuarioController = require('../controllers/UsuarioController');
 
 router.post('/', UsuarioController.createUsuario);
+router.get('/', (req, res) => { UsuarioController.getUsuarios(req, res, false) });
+router.get('/view', (req, res) => { UsuarioController.getUsuarios(req, res, true) });
 router.get('/eu', UsuarioController.getUsuarioAtual);
 router.put('/:id', UsuarioController.updateUsuario);
+router.get('/:id', UsuarioController.getUsuarioById);
+router.patch('/:id/alterarStatus', UsuarioController.alterarStatusUsuario);
 
 
 // O terceiro argumento de getUsuarios é isView, que é false por padrão.
 // Se for 'view', o sistema irá buscar a view no banco.
 // Se for qualquer outra coisa, o sistema irá buscar a tabela no banco.
-router.get('/:type?', (req, res) => {
-    const isView = req.params.type === 'view'
-    UsuarioController.getUsuarios(req, res, isView)
-});
+//router.get('/:type?', (req, res) => {
+//    const isView = req.params.type === 'view'
+//    UsuarioController.getUsuarios(req, res, isView)
+//});
 
-router.get('/:id', UsuarioController.getUsuarioById);
-router.patch('/:id/alterarStatus', UsuarioController.alterarStatusUsuario);
 
 module.exports = router;
