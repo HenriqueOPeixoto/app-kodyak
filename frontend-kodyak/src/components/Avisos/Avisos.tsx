a7306126f0c30b51ea763c0c030f1c11ef3d90ee
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2025-05-20 11:47:15 +0000

    Ajustes Avisos

--- frontend-kodyak/src/components/Avisos/Avisos.tsx
+++ frontend-kodyak/src/components/Avisos/Avisos.tsx
@@ -20,6 +20,7 @@ function Avisos() {
                 <ListItem>Tabela de referência removida da tela de lançamento de itens de pedido;</ListItem>
                 <ListItem>O sistema agora permite informar dados de frete no pedido (retirada ou envio ao cliente);</ListItem>
                 <ListItem>Os valores de frete e pedido agora consideram ICMS, que é informado no cadastro de fretes;</ListItem>
+                <ListItem>O nível de acesso agora determina quais status de pedido são acessíveis ao usuário.</ListItem>
             </List>
             <hr />
             <Typography variant="h6">0.2.1</Typography>
